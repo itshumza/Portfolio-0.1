928c52a7f5cebdf57879d2ad9d93077decdf6977
itshumza <hamzaalee111@gmail.com>
2024-03-04 23:22:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,8 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
